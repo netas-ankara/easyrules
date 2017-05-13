9e30e6c444da9b7af342427f569269ed17e58d3c
cgonul <caglar.gonul.001@gmail.com>
2017-05-13 16:36:42 +0300

    netas-dev Hot fix for the wrong implementation.

--- easyrules-spring-boot-starter/src/test/java/org/easyrules/spring/boot/integration/ARestController.java
+++ easyrules-spring-boot-starter/src/test/java/org/easyrules/spring/boot/integration/ARestController.java
@@ -24,6 +24,7 @@
 package org.easyrules.spring.boot.integration;
 
 import org.easyrules.api.RulesEngine;
+import org.easyrules.core.RulesEngineBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,11 +36,9 @@ public class ARestController {
 	@Autowired
 	private DummyRule dummyRule;
 
-	@Autowired
-	private RulesEngine rulesEngine;
-
 	@RequestMapping ("/greeting")
 	public String greeting(@RequestParam (value = "name", defaultValue = "World") String name) {
+		RulesEngine rulesEngine = RulesEngineBuilder.aNewRulesEngine().build();
 		rulesEngine.registerRule(dummyRule);
 		rulesEngine.fireRules();
 		return String.format("Hello classes behind proxies and easy rules %s", name);
